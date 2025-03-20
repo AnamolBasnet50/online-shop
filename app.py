933bfd59e98eacc33d3916514c2058a6a349051d
anamolbasnet50 <anamolbasnet50@gmail.com>
2025-03-20 07:18:01 +0545

    Fixed port binding issue for Render

--- app.py
+++ app.py
@@ -52,7 +52,8 @@ def enroll():
     conn.close()
     
     return redirect('/')
-
+  
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
